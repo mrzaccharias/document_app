414660439a5cb4c95dc4b41d42b349ea16dfc7e7
Zachary Aster <countmathclub7@gmail.com>
2021-08-24 14:11:46 -0400

    Just a test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/welcome", welcome_get);
+app.use("/", welcome_get);
 
 app.listen(3000, function() {
 	console.log("Listening on port 3000!");
